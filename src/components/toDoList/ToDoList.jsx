cd4d095265a4d29a1fc76dc38bba7f95844d316d
tfan2437 <tfan2437@gmail.com>
2024-06-21 17:29:33 -0400

    Move function update

--- src/components/toDoList/ToDoList.jsx
+++ src/components/toDoList/ToDoList.jsx
@@ -27,10 +27,9 @@ const ToDoList = () => {
   const moveTaskUp = (index) => {
     if (index > 0) {
       const updatedTasks = [...tasks];
-      [updatedTasks[index], updatedTasks[index - 1]] = [
-        updatedTasks[index - 1],
-        updatedTasks[index],
-      ];
+      let temp = updatedTasks[index];
+      updatedTasks[index] = updatedTasks[index - 1];
+      updatedTasks[index - 1] = temp;
       setTasks(updatedTasks);
     }
   };
@@ -38,10 +37,9 @@ const ToDoList = () => {
   const moveTaskDown = (index) => {
     if (index < tasks.length - 1) {
       const updatedTasks = [...tasks];
-      [updatedTasks[index], updatedTasks[index + 1]] = [
-        updatedTasks[index + 1],
-        updatedTasks[index],
-      ];
+      let temp = updatedTasks[index];
+      updatedTasks[index] = updatedTasks[index + 1];
+      updatedTasks[index + 1] = temp;
       setTasks(updatedTasks);
     }
   };
